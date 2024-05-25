1caca899978a2ce0cdf99b315931b9e6f7761e2a
Wahidullah Rahimi <wahidrahimi45@gmail>
2024-05-25 09:56:14 -0700

    Project Setup

---
+++ ChallengeApp/src/test/java/com/wahid/ChallengeApp/ChallengeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wahid.ChallengeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
